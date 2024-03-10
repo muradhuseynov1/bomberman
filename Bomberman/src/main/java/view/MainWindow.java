c5a8bd162131289d0a446e70d76bfd61d2db22ce
canberkarkose <canberk.arkose@outlook.com>
2024-03-10 12:35:30 +0100

    éé

--- Bomberman/src/main/java/view/MainWindow.java
+++ Bomberman/src/main/java/view/MainWindow.java
@@ -1,4 +1,13 @@
 package view;
+import javax.swing.JFrame;
+import engine.Constants.*;
 
-public class MainWindow {
+import static engine.Constants.*;
+
+
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        setTitle(MAIN_WINDOW_TITLE);
+        setSize(MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT);
+    }
 }
