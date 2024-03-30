07188dc0027b7c09efa27e17eaa37128aadc4680
Murad Huseynov <huseynovmurad2002@gmail.com>
2024-03-30 14:36:35 +0100

    Instructions page done, welcome screen update

--- Bomberman/src/App.tsx
+++ Bomberman/src/App.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import { WelcomeScreen } from "./view/WelcomeScreen/WelcomeScreen";
 import { ConfigScreen } from "./view/ConfigScreen/ConfigScreen";
 import './App.css';
+import { InstructionsScreen } from "./view/InstructionsScreen/InstructionsScreen";
  
 function App() {
   return (
     <Routes>
       <Route path="/" element={<WelcomeScreen onStart={() => console.log('Game started!')} />} />
       <Route path="/config" element={<ConfigScreen />} />
+      <Route path="/instructions" element={<InstructionsScreen />} />
     </Routes>
   );
 }
--- Bomberman/src/view/WelcomeScreen/WelcomeScreen.tsx
+++ Bomberman/src/view/WelcomeScreen/WelcomeScreen.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
-import { AccordionSummary, AccordionDetails, Typography } from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { StyledBackground, StyledAccordion, CenteredButton } from './WelcomeScreen.styles';
+import { useNavigate } from 'react-router-dom';
+import { StyledBackground, CenteredButton, InstructionsImage } from './WelcomeScreen.styles';
 import bombermanLogo from '../../assets/bomberman_logo.png';
 import instructionsImage from '../../assets/instructions.png';
-import { useNavigate } from 'react-router-dom';
  
 type WelcomeScreenProps = {
   onStart: () => void;
@@ -12,35 +10,20 @@ type WelcomeScreenProps = {
  
 export const WelcomeScreen = ({ onStart }: WelcomeScreenProps) => {
   const navigate = useNavigate();
-  const [expanded, setExpanded] = useState(false);
- 
-  const handleToggleAccordion = () => {
-    setExpanded(!expanded);
-  };
  
   const handleStart = () => {
     onStart();
-    navigate('/config'); // Navigate to ConfigScreen
+    navigate('/config'); 
   };
- 
+
+  const navigateToInstructions = () => {
+    navigate('/instructions'); 
+  };
+
   return (
     <StyledBackground>
       <img src={bombermanLogo} alt="Bomberman" style={{ width: '50%', marginTop: '40px' }} />
-      <StyledAccordion expanded={expanded} onChange={handleToggleAccordion}>
-        <AccordionSummary
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-          onClick={handleToggleAccordion}
-          expandIcon={<ExpandMoreIcon style={{ display: 'none' }} />}
-        >
-          <img src={instructionsImage} alt="Instructions" style={{ width: '100%', cursor: 'pointer' }} />
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography sx={{ textAlign: 'center' }}>
-            Here are the instructions for the game...
-          </Typography>
-        </AccordionDetails>
-      </StyledAccordion>
+      <InstructionsImage src={instructionsImage} alt="Instructions" onClick={navigateToInstructions} />
       <CenteredButton onClick={handleStart} />
     </StyledBackground>
   );
