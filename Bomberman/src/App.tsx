0841e219850edd482d6a1a22619de900a0b9a517
renkoike-o83aqr <63490262+len548@users.noreply.github.com>
2024-04-01 15:23:57 +0200

    Player stauts screen init, temporary GameWindow + model folder init

--- Bomberman/src/App.tsx
+++ Bomberman/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { WelcomeScreen } from "./view/WelcomeScreen/WelcomeScreen";
 import { ConfigScreen } from "./view/ConfigScreen/ConfigScreen";
+import { GameWindow } from "./view/GameWindow/GameWindow";
 import './App.css';
  
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<WelcomeScreen onStart={() => console.log('Game started!')} />} />
       <Route path="/config" element={<ConfigScreen />} />
+      <Route path="/game-window" element={<GameWindow />} />
     </Routes>
   );
 }
