f03d710a1f212fdf65aa55b148f6dbbee7804821
canberkarkose <canberk.arkose@outlook.com>
2024-04-15 23:23:43 +0200

    fix: clean code

--- Bomberman/src/App.tsx
+++ Bomberman/src/App.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme/InstructionsTheme';
 import './App.css';
-import { GameScreen } from "./view/GameScreen/GameScreen";
-import { WelcomeScreen } from "./view/WelcomeScreen/WelcomeScreen";
-import { ConfigScreen } from "./view/ConfigScreen/ConfigScreen";
-import { InstructionsScreen } from "./view/InstructionsScreen/InstructionsScreen";
-import { GameSettingsProvider } from "./contexts/GameSettingsContext";
+import { GameScreen } from './view/GameScreen/GameScreen';
+import { WelcomeScreen } from './view/WelcomeScreen/WelcomeScreen';
+import { ConfigScreen } from './view/ConfigScreen/ConfigScreen';
+import { InstructionsScreen } from './view/InstructionsScreen/InstructionsScreen';
+import { GameSettingsProvider } from './contexts/GameSettingsContext';
 
 export function App() {
   return (
@@ -17,14 +17,17 @@ export function App() {
           <Route path="/" element={<WelcomeScreen />} />
           <Route path="/config" element={<ConfigScreen />} />
           <Route path="/instructions" element={<InstructionsScreen />} />
-          <Route path="/game/:numOfPlayers" element={
-            <GameScreen
-              playerName="Player One"
-              numBombs={4}
-              powers={['Detonator', 'RollerSkate']}
-              numObstacles={4}
-            />
-          } />
+          <Route
+            path="/game/:numOfPlayers"
+            element={(
+              <GameScreen
+                playerName="Player One"
+                numBombs={4}
+                powers={['Detonator', 'RollerSkate']}
+                numObstacles={4}
+              />
+          )}
+          />
         </Routes>
       </ThemeProvider>
     </GameSettingsProvider>
